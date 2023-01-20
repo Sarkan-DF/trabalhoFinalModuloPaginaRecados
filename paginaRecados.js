b03b28512e13759b910ad536f53e16464df25d05
Igor Matos <sarkan.igor@gmail.com>
2023-01-20 20:10:57 -0300

    Atualização

--- paginaRecados.js
+++ paginaRecados.js
@@ -1,38 +1,53 @@
-let data = JSON.parse(localStorage.getItem('recados')) || [];
-const tableEl = document.getElementById("listaRecados");
+const table = document.getElementById("listaRecados");
+const loggedUser = JSON.parse(sessionStorage.getItem("loggedUser"));
+const allUsers = JSON.parse(localStorage.getItem("allUsers"));
 
-function salvaTabela() {
-    const descricaoRecado = document.getElementById('descricao').value;
-    const detalhamentoRecado = document.getElementById('detalhamento').value;
+function recados() {
+    const descricaoRecado = document.getElementById('descricao');
+    const detalhamentoRecado = document.getElementById('detalhamento');
 
     const objRecado = {
-        descricaoRecado: descricaoRecado,
-        detalhamentoRecado: detalhamentoRecado
+        descricaoRecado: descricaoRecado.value,
+        detalhamentoRecado: detalhamentoRecado.value
     };
-
-    data.push(objRecado);
+    loggedUser.recados.push(objRecado);
+    
     renderTabela();
     saveOnStorage();
-}
+};
+
+function checkLogged() {
+    if (loggedUser.logged) {
+    } else{
+        alert("Usuario não autorizado!\nVocê será redirecionado para a tela de login!");
+        return (window.location.href = "paginaEntrarSistema.html");
+    }
+};
 
 function saveOnStorage() {
-    localStorage.setItem("recados", JSON.stringify(data));
-}
+    sessionStorage.setItem("loggedUser", JSON.stringify(loggedUser));
+
+    const findUser = allUsers.findIndex((valor) => valor.userName === loggedUser.userName);
+
+    allUsers[findUser] = loggedUser;
+
+    localStorage.setItem("allUsers", JSON.stringify(allUsers));
+};
 
 function renderTabela() {
-    tableEl.innerHTML = "";
-    data.map((user, index) => {
+    table.innerHTML = "";
+    loggedUser.recados.map((user, index) => {
         const tr = document.createElement("tr");
         const td1 = document.createElement("td");
         const td2 = document.createElement("td");
         const td3 = document.createElement("td");
         const td4 = document.createElement("td");
         const deleteButton = document.createElement("button");
-        const EditButton = document.createElement("button");
+        const editButton = document.createElement("button");
 
         deleteButton.setAttribute("onClick", `deleteRecado(${index})`);
-        EditButton.setAttribute("onClick", `editaRecado(${index})`);
-        EditButton.setAttribute("class", "button");
+        editButton.setAttribute("onClick", `editaRecado(${index})`);
+        editButton.setAttribute("class", "button");
         deleteButton.setAttribute("class", "button");
 
         tr.appendChild(td1);
@@ -40,30 +55,32 @@ function renderTabela() {
         tr.appendChild(td3);
         tr.appendChild(td4);
         td4.appendChild(deleteButton);
-        td4.appendChild(EditButton);
+        td4.appendChild(editButton);
 
         td1.innerHTML = index + 1;
         td2.innerHTML = user.descricaoRecado;
         td3.innerHTML = user.detalhamentoRecado;
         deleteButton.innerHTML = "Delete";
-        EditButton.innerHTML = "Edit";
-        tableEl.appendChild(tr);
+        editButton.innerHTML = "Edit";
+        table.appendChild(tr);
     });
-}
-renderTabela();
+};
 
 function deleteRecado(index) {
-    data.splice(index, 1);
+    loggedUser.recados.splice(index, 1);
     renderTabela();
     saveOnStorage();
-}
+};
 
 function editaRecado(index) {
     const descricaoRecado = prompt("Digite a nova descrição: ");
-    const detalhamentoRecado = prompt("Digite a nova descrição: ");
-
-    data[index].descricaoRecado = descricaoRecado;
-    data[index].detalhamentoRecado = detalhamentoRecado;
+    const detalhamentoRecado = prompt("Digite o novo detalhamento: ");
+    
+    loggedUser.recados[index].descricaoRecado = descricaoRecado;
+    loggedUser.recados[index].detalhamentoRecado = detalhamentoRecado;
     renderTabela();
     saveOnStorage();
-}
+};
+
+//checkLogged();
+renderTabela();
